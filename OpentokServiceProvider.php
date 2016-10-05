5af2e7aa31bc037ae5fe6ed14c053f2f76e791aa
Marijn de Vries Hoogerwerff <marijn@marinos.nl>
2016-10-05 13:34:30 +0200

    Create OpentokServiceProvider.php

---
+++ OpentokServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace m4rinos\laravel-opentok;
+
+use Opentok\Connection;
+use Illuminate\Support\ServiceProvider;
+
+class OpentokServiceProvider extends ServiceProvider
+{
+
+  /**
+   * Perform post-registration booting of services.
+   *
+   * Adding the config file to the Laravel config directory
+   *
+   * @return void
+   */
+public function boot() {
+	$this->publishes([
+		__DIR__.'/config/opentok.php' => config_path('opentok.php'),
+	]);
+
+	$this->app['OpentokApi'] = $this->app->share(function($app) {
+		return new OpenTok(
+			$app['config']->get('opentok')['api_key'],
+			$app['config']->get('opentok')['api_secret']
+		);
+	});
+	
+}
